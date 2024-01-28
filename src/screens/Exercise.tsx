d0cd7a027ed99377bdd58a3a95748ac36b14e3bd
bernardo.amim <bernardo.amim@mercatus.com>
2024-01-28 18:11:30 -0300

    feat: finalizing exercise details screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,98 @@
-import { Center, Text } from 'native-base'
+import BodySvg from '@assets/body.svg'
+import PullUpImg from '@assets/pullUp.jpg'
+import RepeatSvg from '@assets/repetitions.svg'
+import SeriesSvg from '@assets/series.svg'
+import { Button } from '@components/Button'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import {
+  Box,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+} from 'native-base'
+import { TouchableOpacity } from 'react-native'
 
 export function Exercise() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg={'gray.600'} pt={16}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color={'green.500'} size={6} />
+        </TouchableOpacity>
+
+        <HStack
+          justifyContent={'space-between'}
+          mt={6}
+          mb={4}
+          alignItems={'center'}
+        >
+          <Heading
+            color={'gray.100'}
+            fontSize={'lg'}
+            fontFamily={'heading'}
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems={'center'}>
+            <BodySvg />
+            <Text color={'gray.200'} fontSize={'sm'} fontFamily={'body'} ml={1}>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            w={'full'}
+            h={80}
+            source={PullUpImg}
+            rounded={'md'}
+            mb={3}
+            resizeMode="cover"
+            alt="Nome do exercício"
+          />
+
+          <Box bg={'gray.600'} rounded={'md'} pb={4} px={4}>
+            <HStack
+              alignItems={'center'}
+              justifyContent={'space-around'}
+              mb={6}
+              mt={5}
+            >
+              <HStack alignItems={'center'}>
+                <SeriesSvg />
+                <Text color={'gray.200'} ml={2}>
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack alignItems={'center'}>
+                <RepeatSvg />
+                <Text color={'gray.200'} ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
+    </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,34 @@
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 import { useState } from 'react'
 
 export function Home() {
   const [groups, setGroups] = useState(['costas', 'biceps', 'triceps', 'ombro'])
+  const [selectedGroup, setSelectedGroup] = useState('costas')
   const [exercises, setExercises] = useState([
     'Puxada frontal',
     'Remada curvada',
     'Remada unilateral',
     'Levantamento terra',
   ])
-  const [selectedGroup, setSelectedGroup] = useState('costas')
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise')
+  }
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <FlatList
         data={groups}
-        keyExtractor={(item) => item}
+        keyExtractor={item => item}
         horizontal
         renderItem={({ item }) => (
           <Group
@@ -34,6 +43,7 @@ export function Home() {
         _contentContainerStyle={{ px: 8 }}
         my={10}
         maxH={10}
+        minH={10}
       />
 
       <VStack flex={1} px={8}>
@@ -48,8 +58,10 @@ export function Home() {
 
         <FlatList
           data={exercises}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
